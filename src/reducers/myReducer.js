cd8fb5212128c014537f9144774b9826099e1376
siegfried bozza <siegfried.bozza@protonmail.com>
2022-06-24 15:10:50 +0200

    set up redux

---
+++ src/reducers/myReducer.js
@@ -0,0 +1,12 @@
+export default function myReducer(state, action) {
+  // if (state === undefined) {
+  //   return YYY
+  // }
+
+  // switch (action.type) {
+  //   case 'XXX':
+  //     return action.payload
+  //   default:
+  //     return state;
+  // }
+}
