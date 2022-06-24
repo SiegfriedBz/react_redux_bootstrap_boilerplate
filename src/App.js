c6919095d146cb5824ccfead29f2878ee98cdb46
siegfried bozza <siegfried.bozza@protonmail.com>
2022-06-24 15:00:41 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import '../src/styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>APP</h1>
+      <button className='btn btn-primary'>Click me</button>
+    </div>
+  );
+}
+
+export default App;
